18b52f9b8a42f21762df34d189e15fca0c3ca120
AjithPanneerselvam <ajithpanneerselvam@live.com>
2017-07-21 04:29:27 +0530

    update

--- Field Monitoring/MapViewController.swift
+++ Field Monitoring/MapViewController.swift
@@ -24,20 +24,20 @@ class MapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     
     override func loadView() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
-        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let camera = GMSCameraPosition.camera(withLatitude: 13.357126, longitude: 80.14286, zoom: 6.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
         view = mapView
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
+        marker.position = CLLocationCoordinate2D(latitude: 13.357126, longitude: 80.14286)
+        marker.title = "R.M.K Engineering College"
+        marker.snippet = "India"
         marker.map = mapView
     }
     
