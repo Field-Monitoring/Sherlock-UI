217f5210921ecf409b929dffd267c57201560be4
christopher daniel <christopherdaniel13397@gmail.com>
2017-09-27 21:39:53 +0530

    Update AppDelegate.swift

--- Field Monitoring/AppDelegate.swift
+++ Field Monitoring/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        GMSServices.provideAPIKey("AIzaSyBEI6L_ZmHUkqehHXgXvunSZJj_3lY8B7g")
+        GMSServices.provideAPIKey("")
         return true
     }
 
